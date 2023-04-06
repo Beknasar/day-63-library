93dd2d6419f8355318110b6427369878bad474ec
Beknasar <680633@gmail.com>
2023-04-06 17:25:00 +0300

    Created a new database called book-collection.db Created db Table named Book. Updated requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"{self.title} - {self.author} - {self.rating}"
+
+    db.create_all()
+
 
 all_books = []
 
