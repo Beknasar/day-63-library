0867c6befec06ff2b41519b5d8647d87326549b2
Beknasar <680633@gmail.com>
2023-04-06 18:18:03 +0300

    Created Edit books rating page. Created Edit functions, urls.

--- main.py
+++ main.py
@@ -43,5 +43,15 @@ def add():
     return render_template("add.html")
 
 
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    book_to_update = Book.query.get(id)
+    if request.method == "POST":
+        book_to_update.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", book=book_to_update)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
